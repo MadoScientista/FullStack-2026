7203484a6ed8fd449f1dbe95c9f4544927f0acd8
MadoScientista <samuel.cortes.n@gmail.com>
2026-03-28 23:12:48 -0300

    .

---
+++ prestaCompu/src/main/java/com/example/madoscientista/prestaCompu/PrestaCompuApplication.java
@@ -0,0 +1,13 @@
+package com.example.madoscientista.prestaCompu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestaCompuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestaCompuApplication.class, args);
+	}
+
+}
---
+++ prestaCompu/src/test/java/com/example/madoscientista/prestaCompu/PrestaCompuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madoscientista.prestaCompu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestaCompuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
